f76b12fbb02b7296e2fc800d94161b5f4b8e2c0b
PeanutMutton <peanutmutton@outlook.com>
2023-06-02 10:37:51 +0600

    More or less working game

---
+++ script.js
@@ -0,0 +1,77 @@
+
+choices = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+    //choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+
+
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == 'rock' || playerSelection == "Rock"){
+        if (computerSelection == 'Rock'){
+            return "Draw! Rock vs Rock";
+        }
+        else if (computerSelection == "Paper"){
+            return "You lose! Paper beats Rock";
+        }
+        else if (computerSelection == "Scissors"){
+            return "You win! Rock beats Scissors";
+        }
+    }
+    else if (playerSelection == 'paper' || playerSelection == "Paper"){
+        if (computerSelection == "Rock"){
+            return "You win! Paper beats Rock";
+        }
+        else if (computerSelection == "Paper"){
+            return "Draw! Paper vs Paper";
+        }
+        else if (computerSelection == "Scissors"){
+            return "You lose! Scissors beat Paper"
+        }
+    }
+    else if (playerSelection == "scissors" || playerSelection == "Scissors"){
+        if (computerSelection == "Rock"){
+            return "You lose! Rock beats Scissors"
+        }
+        else if (computerSelection == "Paper"){
+            return "You win! Scissors beat Paper"
+        }
+        else if (computerSelection == "Scissors"){
+            return "Draw! Scissors vs Scissors"
+        }
+    }
+    else {return "Not a valid element"}
+}
+
+
+
+function game(){
+    playerScore = 0;
+    computerScore = 0;
+    roundResult = '';
+    for (let noRounds = 0; noRounds < 6; noRounds++){
+        playerSelection = prompt()
+        roundResult = playRound(playerSelection, getComputerChoice());
+        console.log(roundResult);
+        if(roundResult.includes("You win")){
+            playerScore += 1;
+        }
+        else if (roundResult.includes("You lose")){
+            computerScore += 1;
+        }
+        console.log("Score is " + playerScore + ":" + computerScore)
+        if(playerScore == 3){
+            return("Player wins!");
+        }
+        else if(computerScore == 3){
+            return("AI wins!");
+        }
+        else if (noRounds ==5 && playerScore < 3 && computerScore < 3){
+            console.log("Game over.")
+        }
+    }
+}
+console.log(game());
